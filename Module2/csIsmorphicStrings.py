b292cbc3e48527740883885e829815ee01d89485
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-01-07 00:47:54 -0500

    comments and understanding

--- Module2/csIsmorphicStrings.py
+++ Module2/csIsmorphicStrings.py
@@ -8,13 +8,19 @@ def csIsomorphicStrings(a, b):
     b_list = [0 for _ in range(128)]
     
     for i in range(0, len(a)):
+        # this is a hashmap easy
         a_idx = ord(a[i]);
         b_idx = ord(b[i]);
         
         if a_list[a_idx] != b_list[b_idx]:
             return False
             
-            a_list[a_idx] = i+1
-            b_list[b_idx] = i+1
+        a_list[a_idx] = i+1
+        b_list[b_idx] = i+1
     
     return True
+
+a = "egg"
+b = "add"
+# Output: true
+print(csIsomorphicStrings(a, b))
